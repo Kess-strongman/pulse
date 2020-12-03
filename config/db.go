49a490d69d2ee591cbed96fbfe0b4e41ca245a31
Chloe <chloe@airsensa.com>
2020-12-03 13:59:12 +0000

    chloe mod

--- config/db.go
+++ config/db.go
@@ -1,1 +1,30 @@
 package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	"context"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	pgx "github.com/jackc/pgx/v4/pgxpool"
+)
+
+//DB connection to database
+var DB *pgx.Pool
+
+//DBerr database error
+var DBerr error
+//InitDatabase to create connection
+func InitDatabase(connectionString string) {
+
+	//fmt.Println(connectionString)
+
+	DB, DBerr = pgxpool.Connect(context.Background(), connectionString)
+
+	log.Println("DB Connected")
+	if DBerr != nil {
+		fmt.Println("Unabled to Create DB Connection", DBerr)
+	}
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
+	github.com/jackc/pgx/v4 v4.9.2
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
